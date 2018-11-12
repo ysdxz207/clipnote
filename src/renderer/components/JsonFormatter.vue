111fed085dc8d83dcf05e74d5328124f43ccc822
Moses <ysdxz207@qq.com>
2018-11-12 18:31:30 +0800

    样式

--- src/renderer/components/JsonFormatter.vue
+++ src/renderer/components/JsonFormatter.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>
-        <el-input
-                type="textarea"
-                :autosize="{ minRows: 14, maxRows: 20}"
-                placeholder="请输入内容"
-                v-model="textarea3">
-        </el-input>
-        <el-button type="success">成功按钮</el-button>
+    <div class="el-row">
+        <div :span="24">
+            <el-input
+                    type="textarea"
+                    :autosize="{ minRows: 14, maxRows: 20}"
+                    placeholder="请输入内容"
+                    v-model="json">
+            </el-input>
+        </div>
+        <div :span="24">
+            <el-button type="success">成功按钮</el-button>
+        </div>
     </div>
 </template>
 
@@ -14,6 +18,7 @@
     export default {
         data() {
             return {
+                json: ''
             }
         },
         mounted() {
